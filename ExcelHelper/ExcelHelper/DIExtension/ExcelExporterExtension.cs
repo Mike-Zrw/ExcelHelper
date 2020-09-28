3aa135c512b04fa09d4516f55505b965188489fd
zrw <1836468472@qq.com>
2020-09-28 18:19:32 +0800

    增加netcore service注入

---
+++ ExcelHelper/ExcelHelper/DIExtension/ExcelExporterExtension.cs
@@ -0,0 +1,13 @@
+﻿using ExcelHelper.Exporter;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExcelHelper.DIExtension
+{
+    public static class ExcelExporterExtension
+    {
+        public static void AddExcelExporter(this IServiceCollection service)
+        {
+            service.AddSingleton(typeof(IExcelExporter), typeof(DefaultExcelExporter));
+        }
+    }
+}
---
+++ ExcelHelper/ExcelHelper/DIExtension/ExcelImporterExtension.cs
@@ -0,0 +1,13 @@
+﻿using ExcelHelper.Importer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExcelHelper.DIExtension
+{
+    public static class ExcelImporterExtension
+    {
+        public static void AddExcelImporter(this IServiceCollection service)
+        {
+            service.AddSingleton(typeof(IExcelImporter), typeof(DefaultExcelImporter));
+        }
+    }
+}
